f3bf4bd8fa20cafb1b97913a236363b0d267931e
Inga <ingabr1@hotmail.com>
2022-05-02 19:25:52 +0200

    masse

--- __tests__/movies.test.jsx
+++ __tests__/movies.test.jsx
@@ -1,11 +1,24 @@
+import * as React from "react";
+import {render} from "react-dom";
+import {ListMovies} from "../movieApplication";
+import {Questions} from "../questions";
+
 describe("movie pages", () => {
 
     it("shows movies list", () => {
-        expect("movies").toBe("movies");
+        const element = document.createElement("div");
+        render(<ListMovies />, element);
+        expect(element.innerHTML).toMatchSnapshot();
     });
 
-    it("lets the user add a new movie", () => {
-
-    });
+   /* it("lets the user add a new movie", () => {
+        const question = Questions.find(q => q.id === 995);
+        const answersNames = Object
+            .keys(question.answers)
+            .filter(a => question.answers[a]);
+        expect(answersNames).toEqual([
+            "answer_a", "answer_b", "answer_c", "answer_d"
+        ])
+    });*/
 
 });
--- server/server.js
+++ server/server.js
@@ -1,9 +1,40 @@
 import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(express.static("../client/dist"));
+const MOVIES = [
+    {
+        title: "The Matrix - from server",
+        plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        year: 1999
+    },
+    {
+        title: "The Color Purple - from server",
+        plot: "A black Southern woman (Whoopi Goldberg) struggles to find her identity after suffering years of abuse from her father and others over 40 years.",
+        year: 1985
+    }
+];
 
+app.use(bodyParser.json());
+
+//app.use(express.static("../client/dist"));
+
+app.get("/api/movies", (req, res) => {
+    res.json(MOVIES);
+});
+
+app.post("/api/movies", (req, res) => {
+    const {title, year, plot} = req.body;
+    MOVIES.push({title, year, plot});
+    res.sendStatus(200);
+})
+
+app.use(express.static(path.resolve("..", "client", "dist")));
+app.use((req, res) => {
+    res.sendFile(path.resolve("..", "client", "dist", "index.html"));
+});
 
 //Trenger noe middleware for å fikse at det går an å refreshe en side
 
